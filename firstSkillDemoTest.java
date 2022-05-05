41c3a6859215118518fde4faeaf596568c4cc561
badnanx <98980519+badnanx@users.noreply.github.com>
2022-05-04 21:01:19 -0700

    failed test

---
+++ firstSkillDemoTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class firstSkillDemoTest {
+    @Test
+    public void multiplication() {
+        assertEquals(9, firstSkillDemo.mult(3, 2));
+        // failed test
+    }
+}
